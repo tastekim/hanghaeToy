a9a22de48b4a6b26f09685cc81fea8bea042d7a5
damin0320 <damin0320@gmail.com>
2022-09-02 20:53:55 +0900

    로컬스토리지 검색창 에러 제거

--- static/search.js
+++ static/search.js
@@ -3,6 +3,44 @@ const searchMovie = document.querySelector('#search');
 const favorite = document.querySelector('#register-favorite')
 const removeFavorite = document.querySelector('#remove-favorite')
 
+// localstorage 이름 입력 변수
+const loginForm = document.querySelector("#login-form")
+const loginInput = document.querySelector("#login-form input");
+const loginUser = document.querySelector("#greeting");
+
+
+// localstorage 입력값 제어
+const HIDDEN_CLASSNAME = "hidden"
+const USER_KEY = "user"
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USER_KEY, username);
+    showUserName(username);
+    searchMovie.style.visibility = 'visible'
+}
+
+function showUserName(username) {
+    loginUser.classList.remove(HIDDEN_CLASSNAME);
+    loginUser.innerText = `Hello ${username}`;
+}
+
+const savedUserName = localStorage.getItem(USER_KEY);
+
+if (savedUserName === null) {
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    showUserName(savedUserName);
+    searchMovie.style.visibility = 'visible'
+
+}
+
+
+//searchMovie
+
 $(document).ready(function () {
     showMovie();
 });
